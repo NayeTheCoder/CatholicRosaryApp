18069f56085ff58ecadd1d8e770ca2e06e018281
Nayeli <nayeli585052@gmail.com>
2024-12-02 20:26:32 -0600

    Litany page still working on it

---
+++ src/components/Litanies.jsx
@@ -0,0 +1,100 @@
+import react from 'react';
+import './Litanies.css';
+
+
+const Litanies = () => {
+
+
+    return (
+        <div className='litanies-page'>
+        <h1> Litanies </h1>  
+
+        <h2> The Litany of the Blessed Virgin Mary</h2> 
+        <p>
+        Lord, have mercy on us. 
+Christ, have mercy on us. 
+Lord, have mercy on us. Christ hear us. 
+Christ, graciously hear us. 
+God, the Father of heaven,
+Have mercy on us. 
+God, the Son, Redeemer of the world: 
+Have mercy on us. 
+God, the Holy Ghost, 
+Have mercy on us. 
+Holy Trinity, One God, 
+Have mercy on us. 
+Holy Mary, pray for us. (Repeat at the end of each phrase.) 
+Holy Mother of God, 
+Holy Virgin of virgins, 
+Mother of Christ, 
+Mother of divine grace, 
+Mother most pure, 
+Mother most chaste, 
+Mother inviolate, 
+Mother undefiled, 
+Mother most amiable, 
+Mother most admirable, 
+Mother of good counsel, 
+Mother of our Creator, 
+Mother of our Savior, 
+Virgin most prudent, 
+Virgin most venerable, 
+Virgin most renowned, 
+Virgin most powerful, 
+Virgin most merciful, 
+Virgin most faithful, 
+Mirror of justice, 
+Seat of wisdom, 
+Cause of our joy, 
+Spiritual vessel, 
+Vessel of honor, 
+Singular vessel of devotion, 
+Mystical rose, 
+Tower of David, 
+Tower of ivory, 
+House of gold, 
+Ark of the covenant, 
+Gate of Heaven, 
+Morning star, 
+Health of the sick, 
+Refuge of sinners, 
+Comforter of the afflicted, 
+Help of Christians, 
+Queen of angels, 
+Queen of patriarchs, 
+Queen of prophets, 
+Queen of apostles, 
+Queen of martyrs, 
+Queen of confessors, 
+Queen of virgins, 
+Queen of all saints, 
+Queen conceived without original sin, 
+Queen assumed into heaven, 
+Queen of the most holy Rosary, 
+Queen of peace. 
+Lamb of God, who takest away the sins of the world, 
+Spare us, O Lord. 
+Lamb of God, who takest away the sins of the world, 
+Graciously hear us O Lord. 
+Lamb of God, who takest away the sins of the world, 
+Have mercy on us. 
+V. Pray for us, O holy Mother of God.
+R. That we may be made worthy of the promises of Christ. 
+
+Let us pray: 
+Grant, O Lord God, we beseech Thee, that we Thy servants may rejoice in continual health of mind and body; and, through the glorious intercession of Blessed Mary ever Virgin, may be freed from present sorrow, and enjoy eternal gladness. Through Christ our Lord. Amen. 
+
+
+        </p>
+
+        </div>
+  );
+};
+
+
+export default Litanies;
+
+
+
+// Litany of The Blessed Virgin Mary
+// https://www.ourcatholicprayers.com/litany-of-the-blessed-virgin-mary.html
