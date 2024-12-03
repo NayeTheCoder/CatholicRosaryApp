7e14237428cfeb674a89babfd0150e6768b7f044
Nayeli <nayeli585052@gmail.com>
2024-12-03 07:48:44 -0600

    Added 3 litanies will add more

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,8 +13,9 @@ const Navbar = () => {
           <li><Link to="/CatholicRosaryApp/" className="nav-link">Home</Link></li>
           <li><Link to="/CatholicRosaryApp/long-rosary" className="nav-link">The Holy Rosary With Bible Verses</Link></li>
           <li><Link to="/CatholicRosaryApp/short-rosary" className="nav-link">The Holy Rosary</Link></li>
-          <li><Link to="/CatholicRosaryApp/divine-mercy" ClassName="nav-link"> Divine Mercy Prayer</Link></li>
+          <li><Link to="/CatholicRosaryApp/divine-mercy" className="nav-link"> Divine Mercy Prayer</Link></li>
           <li><Link to="/CatholicRosaryApp/spanish-rosary" className="nav-link">El Santo Rosario</Link></li>
+          <li><Link to="/CatholicRosaryApp/litanies" className="nav-link"> Litanies </Link></li>
           <li><Link to="/CatholicRosaryApp/prayers" className="nav-link">Prayers</Link></li>
           <li><Link to="/CatholicRosaryApp/rosary-facts-spanish" className="nav-link">Informacion Del Santo Rosario</Link></li>
           <li><Link to="/CatholicRosaryApp/rosary-facts" className="nav-link">Rosary Info</Link></li>
