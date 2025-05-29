8c7cf9c3123e6bd12dbe326ce5363c1174e21e15
nazarit1 <nazarit1@iastate.edu>
2025-05-29 12:17:29 -0500

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,39 +8,24 @@ const CustomNavbar = () => {
   return (
     <Navbar expand="lg" bg="light" variant="light">
       <Container>
-        <Navbar.Brand href="#">JESUS IS KING!!!!!</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse className="show" id="navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/CatholicRosaryApp/">Home</Nav.Link>
+        <Navbar.Brand className="mx-auto" href="/">JESUS IS KING!!!!!</Navbar.Brand>
 
-             {/* NavDropdown for English Rosaries */}
-             <NavDropdown title="The Holy Rosary" id="navbar-dropdown-litanies">
-              <NavDropdown.Item as={Link} to="/CatholicRosaryApp/long-rosary">The Holy Rosary With Bible Verses</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/CatholicRosaryApp/short-rosary">The Holy Rosary</NavDropdown.Item>
-            </NavDropdown>
-            {/* NavDropdown for Divine Mercy English/Spanish */}
-            <NavDropdown title ="Divine Mercy Chaplet in English/ en Español">
-              <NavDropdown.Item as={Link} to="/CatholicRosaryApp/divine-mercy">Divine Mercy Chaplet</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/CatholicRosaryApp/divine-mercySpanish">Divina Misericordia Coronilla </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link as={Link} to="/CatholicRosaryApp/spanish-rosary">El Santo Rosario</Nav.Link>
+          <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse className="show" id="navbar-nav">
+              <Nav className="mx-auto">
+                {/* Link to Holy Rosary page */}
+                <Nav.Link as={Link} to="/CatholicRosaryApp/long-rosary">The Holy Rosary</Nav.Link> {/* will lead to its own page */}
+                {/*Link to Divine Mercy Chaplet*/}
+                <Nav.Link as={Link} to="/CatholicRosaryApp/divine-mercy">Divine Mercy Chaplet</Nav.Link> {/* will lead to its own page */}
+                {/* Link to prayers page */}
+                <Nav.Link as={Link} to="/CatholicRosaryApp/prayers">Prayers</Nav.Link>
 
-            {/* NavDropdown for Litanies and Letanias */}
-            <NavDropdown title="Litanies" id="navbar-dropdown-litanies">
-              <NavDropdown.Item as={Link} to="/CatholicRosaryApp/litanies">Litanies</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/CatholicRosaryApp/spanish-litany">Letanias</NavDropdown.Item>
-            </NavDropdown>
-
-            <Nav.Link as={Link} to="/CatholicRosaryApp/prayers">Prayers</Nav.Link>
-
-             {/* NavDropdown for Rosary Information */}
-             <NavDropdown title="Rosary Information" id="navbar-dropdown-litanies">
-              <NavDropdown.Item as={Link} to="/CatholicRosaryApp/rosary-facts">Rosary Info</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/CatholicRosaryApp/rosary-facts-spanish">Informacion Del Santo Rosario</NavDropdown.Item>
-            </NavDropdown>
-      
-            <Nav.Link as={Link} to="/CatholicRosaryApp/resources">Resources</Nav.Link>
+                {/* More dropdown*/}
+                <NavDropdown title="More">
+                  <NavDropdown.Item as={Link} to="/CatholicRosaryApp/litanies">Litanies</NavDropdown.Item> {/* will also have link to toggle eng/span */}
+                  <NavDropdown.Item as={Link} to="/CatholicRosaryApp/resources">Resources</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/CatholicRosaryApp/rosary-facts">Rosary Information</NavDropdown.Item>
+                </NavDropdown> 
           </Nav>
         </Navbar.Collapse>
       </Container>
